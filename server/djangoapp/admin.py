027d4c52d25410c11c6c3a6123c7d6b0c969b87e
kochetovdmytro <kochetovdmytro@gmail.com>
2022-09-25 13:49:39 -0400

    changes made from theia lab environment

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,21 @@
 from django.contrib import admin
-# from .models import related models
+from .models import CarMake, CarModel
 
 
 # Register your models here.
 
 # CarModelInline class
+class CarModelInline(admin.TabularInline):
+    model = CarModel
 
 # CarModelAdmin class
-
+class CarModelAdmin(admin.ModelAdmin):
+    pass
 # CarMakeAdmin class with CarModelInline
-
+class CarMakeAdmin(admin.ModelAdmin):
+    inlines = [
+        CarModelInline,
+    ]
 # Register models here
+admin.site.register(CarModel, CarModelAdmin)
+admin.site.register(CarMake, CarMakeAdmin)
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -9,8 +9,12 @@ from django.utils.timezone import now
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=300)
 
-
+    def __str__(self):
+        return self.name
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
 # - Name
@@ -19,7 +23,20 @@ from django.utils.timezone import now
 # - Year (DateField)
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
-
+class CarModel(models.Model):
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    dealerId = models.IntegerField()
+    name = models.CharField(max_length=30)
+    TYPES = (
+        ('Sedan', "Sedan"),
+        ('SUV', "SUV"),
+        ('WAGON', "WAGON")
+    )
+    Type = models.CharField(max_length=9,
+                    choices=TYPES)
+    year = models.DateField()
+    def __str__(self):
+        return self.name +' ' + self.Type
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
 
