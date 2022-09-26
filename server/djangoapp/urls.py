6ef3afce3c5e7952e84dafcdbff53ee0f58d057e
kochetovdmytro <112124468+kochetovdmytro@users.noreply.github.com>
2022-09-26 13:39:26 +0200

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -26,5 +26,6 @@ urlpatterns = [
     # path for dealer reviews view
 
     # path for add a review view
+    path('dealer/<int:dealer_id>/add_review', views.add_review, name='add_review'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
